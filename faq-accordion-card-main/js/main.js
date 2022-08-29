6063a3e17647d003ecdd3a5f4bc42fff989970ab
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-08-28 21:42:37 -0400

    accordion in-progress, layout updated

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,11 +1,11 @@
-var acc = document.getElementsByClassName("card--accordion");
+var acc = document.getElementsByClassName("accordion__title--wrapper");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    // /* Toggle between adding and removing the "active" class,
+    // to highlight the button that controls the panel */
+    // this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
@@ -14,5 +14,15 @@ for (i = 0; i < acc.length; i++) {
     } else {
       panel.style.display = "block";
     }
+  });
+}
+
+
+var accTitle = document.getElementsByClassName("accordion__title");
+for (i = 0; i < accTitle.length; i++) {
+  accTitle[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("accordion__title--active");
   });
 }
