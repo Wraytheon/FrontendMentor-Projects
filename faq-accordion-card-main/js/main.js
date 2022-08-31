82616b97bf970ad00789d75e32e9785820f6e809
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-08-30 22:28:25 -0400

    updated graphic positioning

--- faq-accordion-card-main/js/main.js
+++ faq-accordion-card-main/js/main.js
@@ -1,7 +1,7 @@
 var acc = document.getElementsByClassName("accordion__title--wrapper");
 var i;
 
-for (i = 0; i < acc.length; i++) {
+for (i = 0; i < 2; i++) {
   acc[i].addEventListener("click", function () {
     // /* Toggle between adding and removing the "active" class,
     // to highlight the button that controls the panel */
